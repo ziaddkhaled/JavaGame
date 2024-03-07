e4d4c623693245721e93938925b0550e437151a4
ziaddkhaled <161713852+ziaddkhaled@users.noreply.github.com>
2024-03-07 16:02:45 +0200

    Add files via upload

---
+++ GameActionException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public abstract class GameActionException extends Exception{
+
+	public GameActionException() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	public GameActionException(String message) {
+		super(message);
+	}
+	
+
+}
